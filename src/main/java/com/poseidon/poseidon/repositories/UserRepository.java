8e798215b56b8f5f7c97850f6c58ba10016687b3
Jerome Cheviet <admin@gharan.ovh>
2022-08-16 12:46:12 +0200

    Configured domain classes and Repository classes to work with database.
    Updated repositories to extend CrudRepository instead of JpaRepository.

--- src/main/java/com/poseidon/poseidon/repositories/UserRepository.java
+++ src/main/java/com/poseidon/poseidon/repositories/UserRepository.java
@@ -3,8 +3,10 @@ package com.poseidon.poseidon.repositories;
 import com.poseidon.poseidon.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-
-public interface UserRepository {//extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
